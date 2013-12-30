a8d37673c05c7f02b88025918f685d0460358ffa
darshan <itzdarshureddy@gmail.com>
2013-12-30 13:31:31 +0530

    image upload

--- src/main/java/com.ArtBunk/controllers/impl/ImageControllerImpl.java
+++ src/main/java/com.ArtBunk/controllers/impl/ImageControllerImpl.java
@@ -9,10 +9,14 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -51,6 +55,24 @@ public class ImageControllerImpl implements ImageController {
         List<Image> images = null;
         images = imageDAO.getImagesWithCriteria(criteria,limit);
         return images;
+    }
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                                                 @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
     }
 
 
